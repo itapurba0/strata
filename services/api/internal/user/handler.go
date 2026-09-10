1ddc185058ec49b9df88a35a8dfc837e231b86e8
Apurba Roy <apurba00237@gmail.com>
2026-09-10 16:11:27 +0530

    refactor user handler to improve request handling and response formatting in Create method

--- services/api/internal/user/handler.go
+++ services/api/internal/user/handler.go
@@ -1,31 +1,49 @@
 package user
 
-import(
-	"context"
+import (
 	"encoding/json"
 	"net/http"
-
 )
 
 type Handler struct {
 	service *Service
 }
 
-func NewHandler(service *Service) *Handler{
+func NewHandler(service *Service) *Handler {
 	return &Handler{service: service}
 }
 
-type createUserRequest struct{
-	Email string `json:"email"`
-	Name string `json:"name"`
+type createUserRequest struct {
+	Email    string `json:"email"`
+	Name     string `json:"name"`
 	Password string `json:"password"`
 }
 
-
-
-func (h *Handler) Create(w http.ResponseWriter, r *http.Request){
+func (h *Handler) Create(w http.ResponseWriter, r *http.Request) {
 	var request createUserRequest
 
 	err := json.NewDecoder(r.Body).Decode(&request)
-	if err != 
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	user, err := h.service.Create(r.Context(), CreateUserInput{
+		Email:    request.Email,
+		Name:     request.Name,
+		Password: request.Password,
+	})
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"id":    user.ID,
+		"email": user.Email,
+		"name":  user.Name,
+	})
 }
